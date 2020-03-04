bc6947b9dd4f0fbe8eb0cc131ca13b5d30ba8449
NamDoTri <trinam9999@gmail.com>
2020-03-04 10:02:19 +0200

    Added search box and search result display

---
+++ src/components/search/SearchResults.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SearchResults = props => {
+    return (
+        <View style={props.style}>
+            <Text>Show Search results</Text>
+        </View>
+    )
+}
+
+export default SearchResults
--- src/components/search/SearchView.js
+++ src/components/search/SearchView.js
@@ -1,12 +1,37 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+
+import SearchBox from './SearchBox'
+import SearchResults from './SearchResults'
 
 const SearchView = () => {
     return (
         <View style={{fontSize:100}}>
             <Text>Search view</Text>
+            <SearchBox 
+                style={styles.SearchBox}
+                handleSubmit={handleSubmit}
+            />
+            <SearchResults style={styles.SearchResults} />
         </View>
     )
 }
 
+const handleSubmit = (type, keyword) => {
+    console.log(type, keyword)
+}
+
 export default SearchView
+
+const styles = StyleSheet.create({
+    container:{
+
+    },
+    searchBox: {
+        flex: 2,
+        backgroundColor: "gray"
+    },
+    searchResult: {
+        flex: 8
+    }
+})
