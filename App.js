3e8e52ba12ef27045801f6360ed2b41e7f644ac4
NamDoTri <trinam9999@gmail.com>
2020-03-04 13:14:27 +0200

    Added baseUri

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import SearchView from './src/components/search/SearchView'
 import ProfileView from './src/components/user/ProfileView'
 
 const Tab = createBottomTabNavigator();
+const baseUri = "http://18.195.169.254:3008";
 
 export default function App() {
   return (
@@ -30,7 +31,9 @@ export default function App() {
 							<FontAwesome name="search" color={color} size={size}/>
 						)
 					}}>
-						{props => <SearchView/>}
+						{props => <SearchView
+							baseUri={baseUri}
+						/>}
 				</Tab.Screen>
 				<Tab.Screen 
 					name="Profile"
