1b14270544bb93a8a15d8fc4043296014abd461a
NamDoTri <trinam9999@gmail.com>
2020-03-06 14:29:06 +0200

    Render search results in a scrollable list

--- components/search/SearchResults.js
+++ components/search/SearchResults.js
@@ -1,20 +1,32 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import ProductCompact from './ProductCompact'
 
 const SearchResults = props => {
     return (
-        <View style={props.style}>
-            {(props.items && props.items.length == 0) ? 
-                <Text>No items found</Text> :
-                props.items.map( item => (
+        <FlatList 
+            style={props.style}
+            data={props.items}
+            renderItem={
+                ({item}) => (
                     <ProductCompact
                         item={item}
-                    />) 
+                    />
                 )
             }
-        </View>
+            keyExtractor={item => item._id}
+        />
     )
 }
 
 export default SearchResults
+
+
+// {(props.items && props.items.length == 0) ? 
+//     <Text>No items found</Text> :
+//     props.items.map( item => (
+//         <ProductCompact
+//             item={item}
+//         />) 
+//     )
+// }
