9cedc04df29575a27936b083586479e578439250
NamDoTri <trinam9999@gmail.com>
2020-03-07 23:03:26 +0200

    Added styling to product detail view

--- screens/ProductView.js
+++ screens/ProductView.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, Image, StyleSheet, Dimensions } from 'react-native'
 
-const ProductView = props => {
-    const [item, setItem] = useState(props.item)
+// TODO: get seller name somehow
 
+const ProductView = props => {
+    const [item, setItem] = useState(props.route.params.item)
+    useEffect(() => {
+        console.log(item)
+    })
     return (
         <View>
             <Image
@@ -11,6 +15,17 @@ const ProductView = props => {
                 loadingIndicatorSource={require("../assets/default_item_placeholder.png")}
                 source={require("../assets/default_item_placeholder.png")}
             />
+            <View style={styles.container}>
+                <View style={styles.brief}>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <Text style={styles.askingPrice}>{item.askingPrice}</Text>
+                </View>
+                <Text style={styles.deliveryType}>Shipping</Text>
+                <Text><Text style={styles.bulletPoint}>Posted by: </Text>{item.seller}</Text> 
+                <Text><Text style={styles.bulletPoint}>Location: </Text>{item.location}</Text>
+                <Text><Text style={styles.bulletPoint}>Description:</Text> {item.description}</Text>
+                <Text><Text style={styles.bulletPoint}>Posted at: </Text> {item.dateOfPosting}</Text>
+            </View>
         </View>
     )
 }
@@ -19,7 +34,7 @@ export default ProductView
 
 const styles = StyleSheet.create({
     container:{
-        flex: 1,
+        paddingLeft: 10,
     },
     images: {
         width: Dimensions.get('window').width,
@@ -27,12 +42,29 @@ const styles = StyleSheet.create({
         resizeMode: 'center',
     },
     brief:{
-
+        paddingBottom: 5,
+        flexDirection: "row"
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        flex: 3,
+    },
+    askingPrice: {
+        fontSize: 30,
+        fontWeight: "bold",
+        flex: 1
     },
     description: {
 
     },
     date: {
         
+    },
+    bulletPoint: {
+        fontWeight: "bold"
+    },
+    deliveryType: {
+        color: "blue"
     }
 })
