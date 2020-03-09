02e6a5415990a466958f1b0cbdd0888dca14a6d0
NamDoTri <trinam9999@gmail.com>
2020-03-09 20:40:13 +0200

    Added template for posting items

--- components/user/PostItem.js
+++ components/user/PostItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, TextInput, Picker, Button } from 'react-native'
 
 const PostItem = props => {
@@ -11,6 +11,11 @@ const PostItem = props => {
         deliveryType: '',
         datePosted: ''
     })
+
+    useEffect(() => {
+        console.log(item)
+    })
+
     return (
         <View>
             <Text>Title</Text>
@@ -22,24 +27,41 @@ const PostItem = props => {
             />
             <Text>Price</Text>
             <TextInput
-                
+                onChange={e => setItem({
+                    price: e.nativeEvent.text,
+                    ...item
+                })}
             />
             <Text>Location</Text>
             <TextInput
-                
+                onChange={e => setItem({
+                    location: e.nativeEvent.text,
+                    ...item
+                })}
             />
             <Text>Delivery Type</Text>
-            <Picker>
+            <Picker
+                onValueChange={e => setItem({
+                    deliveryType: e.nativeEvent.text,
+                    ...item
+                })}
+            >
                 <Picker.Item label="Shipping" value="Shipping" />
                 <Picker.Item label="Pickup" value="Pickup" />
             </Picker>
             <Text>Category</Text>
             <TextInput
-                
+                onChange={e => setItem({
+                    category: e.nativeEvent.text,
+                    ...item
+                })}
             />
             <Text>Description</Text>
             <TextInput
-                
+                onChange={e => setItem({
+                    description: e.nativeEvent.text,
+                    ...item
+                })}
             />
             <Button
                 title="Post"
