ac83f8003672956fda8b9935bbaa182ab5e42cce
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-03-10 09:54:55 +0200

    Design change tests cleared

--- screens/AddItemView.js
+++ screens/AddItemView.js
@@ -7,11 +7,14 @@ const AddItemView = (props) => {
         <React.Fragment>
             <CustomHeader title='Sell' backgroundColor="#d9d9d9"/>
             <View style={style.container}>
-                <TextInput placeholder="name of the item"></TextInput>
-                <TextInput placeholder="description of the item"></TextInput>
-                <TextInput placeholder="price of the item"></TextInput>
-                <TextInput placeholder="pickup or delivery"></TextInput>
-                <TextInput placeholder="location of the item"></TextInput>
+                <View style={style.inputA}>
+                    <Text style={style.textA}>Name</Text>
+                <TextInput placeholder="name of the item" ></TextInput>
+                </View>
+                <TextInput placeholder="description of the item" style={style.inputA}></TextInput>
+                <TextInput placeholder="price of the item" style={style.inputA}></TextInput>
+                <TextInput placeholder="pickup or delivery" style={style.inputA}></TextInput>
+                <TextInput placeholder="location of the item" style={style.inputA}></TextInput>
             </View>
         </React.Fragment>
     )
@@ -26,6 +29,20 @@ const style = StyleSheet.create({
         backgroundColor: "#d9d9d9",
         fontSize: 30
     },
+    textA:{
+        marginBottom: '2%',
+        color: '#3b3c3c'
+        
+    },
+    inputA :{
+        width: '90%',
+        height: 'auto',
+        backgroundColor: 'white',
+        borderRadius: 8,
+        fontSize: 18,
+        padding: '2%',
+        marginTop: '2%'
+    }
 });
 
 export default AddItemView;
